15542f4955167bf4e66270fc3eaa2dff582e01f6
Steffi Michael <steffimic16@gmail.com>
2025-04-19 13:12:45 -0400

    fixed data folder path and removed unwanted files

--- functions/parameters.py
+++ functions/parameters.py
@@ -3,7 +3,7 @@ class Project_Parameters:
     This class will hold parameters or info that can
     be shared across different scripts on the project.
     """
-    data_folder = 'functions/data'
+    data_folder = './data'
     url_dict = {'crime_severity':
                     'https://www150.statcan.gc.ca/n1/tbl/csv/35100026-eng.zip',
                'incident_violation_detail' : 
